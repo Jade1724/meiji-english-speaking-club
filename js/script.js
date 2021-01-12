eaf2ce7072a1711cdf52afa80b5b81443e8de008
Jade1724 <haruka.i1724@gmail.com>
2021-01-12 18:35:06 +1300

    Implemented smooth scrolling and icon animation.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
-  $('.hamburger-menu').on('click', function () {
-
+  $('.hamburger-menu').click(function () {
     $('.animated-icon').toggleClass('show');
   });
 });
+
+$(document).ready(function(){
+  $('main').click(function(){
+  	$('.navbar-toggler').addClass('collapsed');
+    $('.navbar-collaplse').removeClass('show');
+  });
+});
+
+$(document).ready(function () {
+	$('#headingOne').click(function () {
+		$('#chevronOne').toggleClass('rotate')
+	})
+})
+
+$(document).ready(function () {
+	$('#headingTwo').click(function () {
+		$('#chevronTwo').toggleClass('rotate')
+	})
+})
+
+$(document).ready(function () {
+	$('#headingThree').click(function () {
+		$('#chevronThree').toggleClass('rotate')
+	})
+})
+
